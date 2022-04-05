bfb59263627f7fbccb30d9df8f143b684d3f980d
Vitaly Efremov <garant101@yandex.ru>
2022-04-05 20:23:55 +0300

    выгрузка исправлений проекта после ревью в ветку

--- scripts/index.js
+++ scripts/index.js
@@ -1,44 +1,29 @@
 let openPopupBtn = document.querySelector('.profile__btn-edit');
-let noScroll = document.querySelector('.js-root');
-let popup = document.querySelector('.js-popup');
-let closeFormBtn = document.querySelector('.js-popup__close-form');
-let popupForm = document.querySelector('.js-popup__form');
-let popupInputName = document.querySelector('.js-popup__input-name');
-let popupInputJob = document.querySelector('.js-popup__input-job');
+let popup = document.querySelector('.popup');
+let closeFormBtn = document.querySelector('.popup__close-form');
+let popupForm = document.querySelector('.popup__form');
+let popupInputName = document.querySelector('.popup__input_name');
+let popupInputJob = document.querySelector('.popup__input_job');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 
 function openPopup() {
-    popup.classList.add('popup_active');
-    noScroll.classList.add('root_no-scroll');
-    document.addEventListener('keyup', onDocumentKeyUp);
+  popup.classList.add('popup_active');
+  popupInputName.value = profileTitle.textContent;
+  popupInputJob.value = profileSubtitle.textContent;
 }
 
 function closeForm() {
     popup.classList.remove('popup_active');
-    noScroll.classList.remove('root_no-scroll');
-    document.removeEventListener('keyup', onDocumentKeyUp);
 }
-
-function onDocumentKeyUp(event) {
-  if (event.key === ESK_KEY) {
-    closeForm();
-  }
-}
-
-popupInputName.value = profileTitle.textContent;
-popupInputJob.value = profileSubtitle.textContent;
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    let InputName = popupInputName.value;
-    let InputJob = popupInputJob.value;
-    profileTitle.textContent = InputName;
-    profileSubtitle.textContent = InputJob;
+    profileTitle.textContent = popupInputName.value;
+    profileSubtitle.textContent = popupInputJob.value;
+    closeForm();
 }
 
 openPopupBtn.addEventListener('click', openPopup);
 closeFormBtn.addEventListener('click', closeForm);
 popupForm.addEventListener('submit', formSubmitHandler);
-
-const ESK_KEY = 'Escape';
